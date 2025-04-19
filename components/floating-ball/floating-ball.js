48d499f1d26a92aa9bfa3744b89671d78058c974
LuckeyMouse <luckeymouse@outlook.com>
2025-04-19 16:26:54 +0800

    work from 4.19

---
+++ components/floating-ball/floating-ball.js
@@ -0,0 +1,10 @@
+// components/floating-ball/floating-ball.js
+Component({
+  methods: {
+    onTap() {
+      wx.switchTab({
+        url: '/pages/index/index',
+      });
+    }
+  }
+});
---
+++ pages/test/test.js
@@ -0,0 +1,5 @@
+// pages/test/test.js
+Page({
+  data: {},
+  onLoad() {},
+});
